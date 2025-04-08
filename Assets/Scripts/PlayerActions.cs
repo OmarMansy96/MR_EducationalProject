3389ceda8268778d31dfd608565476e37d8b8423
Omar Mansy <omarmansy96@gmail.com>
2025-04-08 12:50:01 +0200

    save for update_2

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 
 using UnityEngine.InputSystem;
 
-public class PlayerActions : MonoBehaviour
+using Unity.Netcode;
+
+public class PlayerActions : NetworkBehaviour
 {
-    public GameObject objectToSpawn; // Prefab to spawn
-    public UnityEngine.XR.Interaction.Toolkit.Interactors.XRRayInteractor rayInteractor; // XR Ray Interactor from the XR Controller
-    public InputActionProperty triggerAction; // XR Controller trigger input
-    public Transform spawnParent; // Optional parent for organization
+    //public GameObject objectToSpawn; // Prefab to spawn
+    //public UnityEngine.XR.Interaction.Toolkit.Interactors.XRRayInteractor rayInteractor; // XR Ray Interactor from the XR Controller
+    //public InputActionProperty triggerAction; // XR Controller trigger input
+    //public Transform spawnParent; // Optional parent for organization
     public GameObject canvas; // Assign the Canvas in Inspector
     private bool isCanvasVisible = false;
 
@@ -16,37 +18,41 @@ public class PlayerActions : MonoBehaviour
     private void Start()
     {
         canvas = GameObject.FindGameObjectWithTag("ControllerUI").gameObject;
-        canvas.SetActive(false);
+        if (!IsServer)
+        {
+            canvas.SetActive(false);
+
+        }
         //canvas = FindFirstObjectByType<Canvas>().gameObject;
     }
     private void Update()
     {
-        if (rayInteractor == null || objectToSpawn == null || triggerAction.action == null)
-            return;
+        //if (rayInteractor == null || objectToSpawn == null || triggerAction.action == null)
+        //    return;
 
-        if (triggerAction.action.WasPressedThisFrame()) // Detect trigger press
-        {
-            TrySpawnObject();
-        }
-        if (menuButtonAction.action.WasPressedThisFrame())
+        //if (triggerAction.action.WasPressedThisFrame()) // Detect trigger press
+        //{
+        //    TrySpawnObject();
+        //}
+        if (IsServer && menuButtonAction.action.WasPressedThisFrame())
         {
             ToggleCanvas();
         }
     }
 
-    private void TrySpawnObject()
-    {
-        if (rayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit)) // Check if ray hits a collider
-        {
-            if (hit.collider != null) // Ensure we hit an actual collider
-            {
-                Vector3 spawnPosition = hit.point;
-                Quaternion spawnRotation = Quaternion.LookRotation(hit.normal); // Aligns with the hit surface
+    //private void TrySpawnObject()
+    //{
+    //    if (rayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit)) // Check if ray hits a collider
+    //    {
+    //        if (hit.collider != null) // Ensure we hit an actual collider
+    //        {
+    //            Vector3 spawnPosition = hit.point;
+    //            Quaternion spawnRotation = Quaternion.LookRotation(hit.normal); // Aligns with the hit surface
 
-                Instantiate(objectToSpawn, spawnPosition, spawnRotation, spawnParent);
-            }
-        }
-    }
+    //            Instantiate(objectToSpawn, spawnPosition, spawnRotation, spawnParent);
+    //        }
+    //    }
+    //}
 
     void ToggleCanvas()
     {
